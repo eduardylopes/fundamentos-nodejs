68e323d5e5a317ea23a3decb9f9a2b63aa1a47fc
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-04-03 11:05:58 -0300

    Included GET method

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(express.json());
 
-const costumers = [];
+const customers = [];
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
-  const customerAlreadyExists = costumers.some(
+  const customerAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
   );
 
@@ -18,14 +18,26 @@ app.post('/account', (request, response) => {
     return response.status(400).json({ error: 'Customer already exists!' });
   }
 
-  costumers.push({
+  customers.push({
     cpf,
     name,
     id: uuidv4(),
-    statment: [],
+    statement: [],
   });
 
   return response.status(201).send();
+});
+
+app.get('/statement/', (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((costumer) => costumer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found!' });
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);

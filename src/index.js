b471013509e1ef70990f771e82119ee73d7f6b99
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-04-03 13:28:00 -0300

    Included Middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((costumer) => costumer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found!' });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -28,16 +43,28 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement/', (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((costumer) => costumer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found!' });
-  }
+app.get('/statement/', verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+  console.log(request);
 
   return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    createdd_at: new Date(),
+    type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
